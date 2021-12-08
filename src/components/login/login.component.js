56daa0512a1c6ecc4c93171f85ef1e50f5748c6e
Jay Naik <jayjonas1996@gmail.com>
2021-12-07 19:35:23 -0600

    Fix login component

--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -15,6 +15,8 @@ export default class LoginComponent extends Component {
         if (this.loginID && this.state.errors.employeeId === '') {
             LocalStorage.set('userId', this.loginID);
             this.getUser();
+        } else {
+            this.updateInputValue(this.loginID);
         }
     }
 
@@ -32,7 +34,7 @@ export default class LoginComponent extends Component {
     updateInputValue(value) {
         const regex = RegExp('[0-9]+')
         let errors = this.state.errors;
-        if (value === '' || value === null || value || undefined) {
+        if (value === '' || value === null || value === undefined) {
             errors.employeeId =  'Employee Id is required';
         } else if (value < 1) {
             errors.employeeId = 'Employee Id cannot be negative or 0';
@@ -51,7 +53,7 @@ export default class LoginComponent extends Component {
             <div className='login-wrapper'>
                 <div className='login-form'>
                     <span className='login-title'>Employee Login</span> <br/>
-                    <input className='login-input' type='number' id="user-id" onChange={e => this.updateInputValue(e.target.value)}/> <br/>
+                    <input className='login-input' type='number' id="user-id" onChange={(e) => this.updateInputValue(e.target.value)}/> <br/>
                     <button className='login-btn' onClick={evt => this.login()}>Login</button>
                     <br/>
                     {this.state.errors.employeeId && <span className='error'>{this.state.errors.employeeId}</span>}
